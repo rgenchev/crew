4def4a217bdba35a722615be2499c1b3227df536
rgenchev <rgenchev@hey.com>
2021-04-25 18:35:14 +0300

    Initial User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
 
-  belongs_to :company
-
-  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should be valid" do
+    assert users(:valid).valid?
+  end
+
+  test "should not be valid without email" do
+    user = users(:valid)
+    user.email = nil
+
+    refute user.valid?
+  end
 end
