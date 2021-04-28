750b465332a58878f0b9fe89787b88ed90bd0c3a
rgenchev <rgenchev@hey.com>
2021-04-28 16:29:02 +0300

    ApplicationHelper: create a helper for determining classes for the flash messages by a given alert level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_message_classes_for_alert_level(value)
+    case value
+    when 'notice' then "blue bg-washed-blue"
+    when 'success' then "green bg-washed-green"
+    when 'error' then "red bg-washed-red"
+    when 'alert' then "gold bg-washed-yellow"
+    end
+  end
 end
