6d700796d50685172f6e3a02fc45b83dd445ed16
rgenchev <rgenchev@hey.com>
2021-04-28 16:53:13 +0300

    AboutControllerTest: test 'index' action

--- test/controllers/about_controller_test.rb
+++ test/controllers/about_controller_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class AboutControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index if logged in" do
+    login(users(:valid))
+    get about_url
+    assert_response :success
+  end
+
+  test "should not get index if not logged in" do
+    get about_url
+    assert_redirected_to login_url
+  end
 end
