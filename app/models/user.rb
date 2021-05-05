ec0e14fb146508548660b6a7a1148af3b7871a57
rgenchev <rgenchev@hey.com>
2021-05-05 14:36:21 +0300

    User: enumerize 'role' and add a presence validation for it

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
 
   belongs_to :team
 
+  enum role: { user: 0, admin: 1 }
+
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :username, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
+  validates :role, presence: true
 
   def username=(value)
     super(value&.downcase)
