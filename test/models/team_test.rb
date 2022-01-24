d8c2e2eec4bcc35d0eea540ddbb32ecbe1c16706
Radoslav Genchev <rgenchev@hey.com>
2022-01-24 11:55:27 +0200

    Test Team model

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @team = teams(:development)
+  end
+
+  test "should be valid" do
+    assert @team.valid?
+  end
+
+  test "should not be valid without name" do
+    @team.name = nil
+
+    refute @team.valid?
+  end
+
+  test "should not be valid with duplicate name" do
+    dup_team = @team.dup
+
+    refute dup_team.valid?
+
+    assert dup_team.errors.added?(:name, 'has already been taken')
+  end
 end
