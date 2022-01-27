d740dbe8724d711dc61ea532dadf4e28ccf75900
Radoslav Genchev <rgenchev@hey.com>
2022-01-27 17:57:35 +0200

    Admin::ApplicationController: add an 'Admin' breadcrumb

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,14 @@
 class Admin::ApplicationController < ApplicationController
   before_action :authorize_admins
+  before_action :set_admin_breadcrumb
+  skip_before_action :set_crumbs
 
   private
     def authorize_admins
       redirect_to root_path and return unless current_user.admin?
+    end
+
+    def set_admin_breadcrumb
+      add_breadcrumb "Admin", admin_dashboard_url
     end
 end
